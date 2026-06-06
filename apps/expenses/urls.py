aa6d43a516325ffa59c8ff5fbbcd81f0512d0bba
Rashid-C <anurashid11055@gmail.com>
2026-06-06 15:03:54 +0530

    Views Serializer updated to from explisit mode to implisit mode

--- apps/expenses/urls.py
+++ apps/expenses/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ExpenseListCreateView
+from .views import ExpenseListCreateView,ExpenseDetailView
 
 
 urlpatterns = [
     path('expenses/', ExpenseListCreateView.as_view(),name='expense-list-create')
+    path('expenses/<int:pk>/', ExpenseDetailView.as)
 ]
--- apps/expenses/views.py
+++ apps/expenses/views.py
@@ -1,8 +1,9 @@
-from rest_framework import generics
+from rest_framework import viewsets
 from .models import Expense
 from .serializers import ExpenseSerializer
 
-class ExpenseListCreateView(generics.ListCreateAPIView):
+
+class ExpensesViewSet(viewsets.ModelViewSet):
     queryset=Expense.objects.all()
     serializer_class=ExpenseSerializer
     
