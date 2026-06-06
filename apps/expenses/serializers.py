a29c39c1a22cecd37a72da09330783096c106a7e
Rashid-C <anurashid11055@gmail.com>
2026-06-06 16:05:52 +0530

    Add Data Validation

--- apps/expenses/serializers.py
+++ apps/expenses/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Expense
+from django.utils import timezone
 
 
 class ExpenseSerializer(serializers.ModelSerializer):
@@ -7,3 +8,13 @@ class ExpenseSerializer(serializers.ModelSerializer):
     class Meta:
         model=Expense
         fields=['id','title','amount','date','category','created_at','user']
+        
+    def validate_amount(self,value):
+        if value <=0:
+            raise serializers.ValidationError('The amount must be a positive number')
+        return value
+    
+    def validate(self,data):
+        if data['date']>timezone.now():
+            raise serializers.ValidationError({"date":"You can't log an expense in future."})
+        return data
