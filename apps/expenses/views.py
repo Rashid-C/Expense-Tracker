c1773341adcfe0253ecbab26fbb505bdcd322ce6
Rashid-C <anurashid11055@gmail.com>
2026-06-06 15:56:49 +0530

    Add Filtering and pagination

--- apps/expenses/views.py
+++ apps/expenses/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets,permissions
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Expense
 from .serializers import ExpenseSerializer
 
@@ -7,5 +8,12 @@ class ExpenseViewSet(viewsets.ModelViewSet):
     serializer_class = ExpenseSerializer
     permission_classes=[permissions.IsAuthenticated] #logic of auth
     
+    filter_backends=[DjangoFilterBackend]
+    filterset_fields=['category','date']
+    
+    
     def get_queryset(self):
         return Expense.objects.filter(user=self.request.user)       # Opti: Only show expenses belonging to the current user
+    
+    
+    
--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
   # 3rd party
     'rest_framework',
     'rest_framework.authtoken',
+    'django_filters',
 
   #local app
     'apps.expenses',
@@ -130,4 +131,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+   
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
