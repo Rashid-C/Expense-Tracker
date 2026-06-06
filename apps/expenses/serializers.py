8509ff8035b3a7f108d9b5acc8d839f08961fee9
Rashid-C <anurashid11055@gmail.com>
2026-06-06 15:43:32 +0530

    Add Authentication

--- apps/expenses/serializers.py
+++ apps/expenses/serializers.py
@@ -3,6 +3,7 @@ from .models import Expense
 
 
 class ExpenseSerializer(serializers.ModelSerializer):
+    user=serializers.HiddenField(default=serializers.CurrentUserDefault())
     class Meta:
         model=Expense
-        fields=['id','title','amount','date','category','created_at']
+        fields=['id','title','amount','date','category','created_at','user']
--- apps/expenses/views.py
+++ apps/expenses/views.py
@@ -1,7 +1,11 @@
-from rest_framework import viewsets
+from rest_framework import viewsets,permissions
 from .models import Expense
 from .serializers import ExpenseSerializer
 
 class ExpenseViewSet(viewsets.ModelViewSet):
     queryset = Expense.objects.all()
     serializer_class = ExpenseSerializer
+    permission_classes=[permissions.IsAuthenticated] #logic of auth
+    
+    def get_queryset(self):
+        return Expense.objects.filter(user=self.request.user)       # Opti: Only show expenses belonging to the current user
